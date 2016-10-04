b541c71f5c47251fd75b4914391e8a99024b5caa
FrancisStephen Quaratiello <FrancisStephen.Quaratiello@pega.com>
2016-10-04 17:23:58 -0400

    new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+apt-get update && apt-get -y upgrade
+apt-get -y autoremove netcat-openbsd
+apt-get -y install python.pip cmake xmonad xmobar suckless-tools vim mate-desktop-environment exuberant-ctags bash cowsay fortune nethack-console screen tmux netcat-traditional nmap bsdgames xscreensaver cabal-install 
